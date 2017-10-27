9a522d48c19352d852c84c47c05e72f8bb7fc701
Alan Gardner <urfolomeus@gmail.com>
2017-10-27 11:35:13 +0100

    Adding simple git branch info

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,6 +3,10 @@ function _print_path
   printf '%s' (string join '/' $cwd)
 end
 
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
 function _print_prompt_char
   printf '⋊> '
 end
@@ -14,7 +18,9 @@ function fish_prompt
   set -l blue (set_color blue)
   set -l green (set_color green)
   set -l red (set_color red)
+  set -l yellow (set_color yellow)
 
+  set -l branch_color $yellow
   set -l dir_color $blue
 
   set -l prompt_color $red
@@ -23,7 +29,7 @@ function fish_prompt
   end
 
   echo -e ''
-  echo -e -n -s $dir_color (_print_path) $normal
+  echo -e -n -s $dir_color (_print_path) ' ' $branch_color (_git_branch_name) $normal
   echo -e ''
   echo -e -n -s $prompt_color (_print_prompt_char) $normal
 end
